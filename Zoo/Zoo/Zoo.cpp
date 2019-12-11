19badffbc95fe485a6c3741129b12f689bab66d3
Fekete Márk <fmark98@gmail.com>
2019-12-11 16:17:20 +0100

    1st tasx finished, ready for tests.

--- Zoo/Zoo/Zoo.cpp
+++ Zoo/Zoo/Zoo.cpp
@@ -2,7 +2,37 @@
 
 using namespace std;
 
+const int MAX_N = 100;
+const int MAX_M = 100;
+const int MAX_K = 10000;
+
+struct Animal
+{
+	//int animalID = -1; index represents it.
+	int animalCount = 0;
+};
+
+struct Zoo
+{
+	//int zooID = -1; index represents it.
+	int animalCount = 0;
+	Animal animals[MAX_M];
+};
+
+Zoo zoos[MAX_N];
+int n, m, k;
+
 int main()
 {
-    cout << "Hello World!\n";
+	cin >> n >> m >> k;
+
+	for (int i = 0; i < n; ++i)
+	{
+		int zooIndex, animalIndex, animalCount;
+		cin >> zooIndex >> animalIndex >> animalCount;
+		zoos[zooIndex - 1].animals[animalIndex - 1].animalCount += animalCount;
+		zoos[zooIndex - 1].animalCount += animalCount;
+	}
+
+	return 0;
 }
