9504bb435172572e933141c86ac05d94ad92ebf9
Fekete Márk <fmark98@gmail.com>
2019-12-11 17:20:10 +0100

    half of the 2nd task is done.

--- Zoo/Zoo/Zoo.cpp
+++ Zoo/Zoo/Zoo.cpp
@@ -22,10 +22,13 @@ struct Zoo
 Zoo zoos[MAX_N];
 int n, m, k;
 
+int animalRareityInZoos[MAX_M];
+
 int main()
 {
 	cin >> n >> m >> k;
 
+	// Reading //
 	for (int i = 0; i < k; ++i)
 	{
 		int zooIndex, animalIndex, animalCount;
@@ -34,6 +37,7 @@ int main()
 		zoos[zooIndex - 1].animalCount += animalCount;
 	}
 
+	// 1st task implementation //
 	int maxAnimalCount = -1;
 	int maxAnimalCountIndex = 0;
 	for (int i = 0; i < n; ++i)
@@ -45,19 +49,59 @@ int main()
 		}
 	}
 
+	// 2nd task implementation //
+	// Generating array //
+	for (int i = 0; i < m; ++i)
+	{
+		animalRareityInZoos[i] = 0;
+		for (int j = 0; j < n; ++j)
+		{
+			if (zoos[j].animals[i].animalCount != 0)
+			{
+				++animalRareityInZoos[i];
+			}
+		}
+	}
+
+	// Selecting minimum //
+	int minAnimalRareityInZoos = 101;
+	int minAnimalRareityInZoosIndex = -1;
+	for (int i = 0; i < MAX_M; ++i)
+	{
+		if (animalRareityInZoos[i] < minAnimalRareityInZoos && animalRareityInZoos[i] != 0)
+		{
+			minAnimalRareityInZoos = animalRareityInZoos[i];
+			minAnimalRareityInZoosIndex = i;
+		}
+	}
+
+	// Counting minimum valus in the given index //
+	int minAnimalRareityInZoosCount = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (zoos[i].animals[minAnimalRareityInZoosIndex].animalCount == minAnimalRareityInZoos)
+		{
+			++minAnimalRareityInZoosCount;
+		}
+	}
 
 
-
-
-
-
-
-
+	// 1st task output //
 	cout << "#" << endl;
 	cout << maxAnimalCountIndex + 1 << endl;
+
+	// 2st task output //
 	cout << "#" << endl;
-	cout << "#" << endl; 
+	cout << minAnimalRareityInZoosCount << endl;
+
+	// 3st task output //
 	cout << "#" << endl;
+
+	// 4st task output //
 	cout << "#" << endl;
+
+	// 5st task output //
+	cout << "#" << endl;
+
 	return 0;
 }
